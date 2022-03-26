f8bf3c66ce8d875f2ffb0100147c64bceb5b57c1
droman892 <droman892@gmail.com>
2022-03-25 23:28:12 -0400

    updated

--- src/Example.js
+++ src/Example.js
@@ -1,16 +1,3 @@
-export const Example = () => <div>Ashley</div>;
-
-
-// export const Example = {
-
-//     customer: { 
-//         firstName: 'Ashley', 
-//         lastName: 'Jones', 
-//         phoneNumber: '(123) 555-0123'
-//     },
-//     stylist: 'Jay Speares',
-//     startsAt: '2019-02-02 09:30',
-//     service: 'Cut',
-//     notes: ''
-
-// }
+export const Example = ({customer}) => (
+    <div>{customer.firstName}</div>
+);
