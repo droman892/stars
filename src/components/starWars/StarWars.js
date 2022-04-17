f386ca1c950c919bac1f97d77c8ea1f942257b5a
droman892 <droman892@gmail.com>
2022-04-17 19:19:25 -0400

    updated

--- src/components/starWars/StarWars.js
+++ src/components/starWars/StarWars.js
@@ -43,17 +43,13 @@ export const StarWars = () => {
                         <div id="crawl-text-container" className="sw-yellow">
                             <p className="text-center">Episode I</p>
                             <p className="text-center">THE DROMAN</p>
-                            <p> Havoc struck the galaxy in a most peculiar way.  One day, Bergumo, the star of Constellation Alonzaz Major, became a supernova, SN 2050Z. </p>
-                            <p> The shockwave created by the nuclear fusion was unlike anything anyone in the known universe ever witnessed before.  In the blink of an eye, all knowledge from the universally-renown movie series, Star Wars, disappeared from existence. </p>
-                            <p> This moment in history became known as The Droman.  Since then, all species have experienced a subtle frustration, a craving to satisfy their desires from quality entertainment.  The growing dissatisfaction from inadequate amusement has led beings all over the universe to resort to looting, murder, and war. </p>
-                            <p> While no knowledge existed of Star Wars in any form, software engineers have comes together from every galaxy to relieve the entertainment frustration once and for all. </p>
-                            <p> Over the decades, every inch in the universe has been investigated as part of the universal effort to reverse The Droman.  Thankfully, the lifetime endeavor regained hope upon finding the nebula left behind from SN 2050Z. </p>
-                            <p> With technology having rapidly developed to overcome The Droman, the software engineers who discovered the nebula were able to extract data from it. </p>
-                            <p> More so, the first data extracted and decrypted from the nebula was knowledge of The Force.  In fact, upon regaining knowledge on this one piece of information, the entertainment vacuum of the some of the engineers was filled so greatly that their brains detonated. Interestingly enough, each one of them were crying in joy during their last seconds living. </p>
-                            <p> Since the rediscovery of the The Force appeared to be too strong as the first bit of information received by someone, other data has been extracted to slowly delivered to the masses in hopes of one day fully restoring their knowledge of Star Wars. </p>
-                            <p> The effort to reverse The Droman continues.  As you'll soon see, the great software engineers have devised an application for you and anyone else in the universe to ingest knowledge of Star Wars at their leisure. </p>
-                            <p> Learn as much as you can while there's time.  There's no telling if or when The Droman will return... </p>
+                            <p> Havoc struck the galaxy in a most peculiar way.  One day, Bergumo, the star of Constellation Alonzaz Major, became a supernova, SN 2050Z. The shockwave created by the nuclear fusion was unlike anything anyone in the known universe ever witnessed before.  In the blink of an eye, all knowledge from the universally renown movie series, Star Wars, disappeared from existence. </p>
+                            <p> This moment in history became known as The Droman.  Since then, all species have experienced a subtle frustration, a craving to satisfy their desires for quality entertainment, which has led to looting, murder, and war. </p>
+                            <p> It took over half a century, but software engineers managed to extract the original Star Wars data from SN 2050Z's nebula and provide a way for the masses to satisfy their entertainment itch.  Learn as much as you can while there's time.  There's no telling if or when The Droman will return...</p>
                         </div>
+
+
+                        
                     </div>
 
 
